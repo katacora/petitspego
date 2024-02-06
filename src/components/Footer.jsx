faeb4d431cb6f38e44d987509d42ee4205df3edd
katacora <claudine.delgado@gmail.com>
2024-02-06 17:55:07 +0100

    first commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="bg-gray-800 text-white p-4 ">
+      <div className="container mx-auto text-center">
+        <p className="text-sm">© 2024 PetitsPego. Todos los derechos reservados.</p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
